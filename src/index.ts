24a9b116e1807dcf31add0de65e4cd3346085bf0
Celso Oliva Aparecido <celso.oliva.aparecido@gmail.com>
2022-05-02 18:41:42 -0300

    chore: rename client folder

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,25 @@
-import express from 'express';
+import express, { NextFunction, Request, RequestHandler, Response } from 'express';
+import bodyParser from 'body-parser';
+import cors, { CorsOptions } from 'cors';
 import { StatusCodes } from 'http-status-codes';
 
+const PORT = process.env.SRV_PORT || '5000';
+
+const allowedOrigins = ['http://localhost:3000'];
+const options: cors.CorsOptions = {
+  origin: allowedOrigins,
+};
+
 const app = express();
+app.use((cors as (opt: CorsOptions) => RequestHandler)(options));
 
-app.use(express.json());
+app.use((_req: Request, res: Response, next: NextFunction) => {
+  res.header('X-powered-by', 'Blood, sweat, and tears.');
+  next();
+});
 
-const PORT = process.env.SRV_PORT || 5000;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.status(StatusCodes.OK).send('GRX Back-end Challenge');
