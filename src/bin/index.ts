7dfdc31141fc6f1c5ca4f4ad5d58c91a3fb9b508
Celso Oliva Aparecido <celso.oliva.aparecido@gmail.com>
2022-05-03 13:20:33 -0300

    chore: moving express server listenning boilerplate to bin folder

---
+++ src/bin/index.ts
@@ -0,0 +1,23 @@
+import { Server } from 'http';
+import { Express } from 'express';
+
+const DEFAULT_PORT = '5000';
+let server: Server;
+const startServer = (app: Express) => {
+  const PORT = process.env.SRV_PORT || DEFAULT_PORT;
+
+  server = app.listen(PORT, () => {
+    console.log(`Server is running... Listening on ${PORT}`);
+  });
+};
+
+process.on('SIGTERM', () => {
+  // Permite o encerramento não abrupto do servidor ao receber o comando
+  // de encerramento do shell tentando responder às requisições correntes
+
+  server.close(() => {
+    console.log('Process terminated by SIGTERM');
+  });
+});
+
+export default startServer;
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,27 @@
-import express, { NextFunction, Request, RequestHandler, Response } from 'express';
+import express, {
+  NextFunction, Request, RequestHandler, Response,
+} from 'express';
 import bodyParser from 'body-parser';
 import cors, { CorsOptions } from 'cors';
 import { StatusCodes } from 'http-status-codes';
-
-const PORT = process.env.SRV_PORT || '5000';
-
-const allowedOrigins = ['http://localhost:3000'];
-const options: cors.CorsOptions = {
-  origin: allowedOrigins,
-};
+import startServer from './bin';
 
 const app = express();
-app.use((cors as (opt: CorsOptions) => RequestHandler)(options));
+
+// CORS
+
+const allowedOrigins = ['http://localhost:3000']; // endereço do cliente react
+const corsOpts: cors.CorsOptions = {
+  origin: allowedOrigins,
+};
+app.use((cors as (opt: CorsOptions) => RequestHandler)(corsOpts));
+
+// SETUP
 
 app.use((_req: Request, res: Response, next: NextFunction) => {
+  // Evita ataques de enumeração para extrair informações,
+  // escondendo 'Express 4.18.1' do header das responses
+
   res.header('X-powered-by', 'Blood, sweat, and tears.');
   next();
 });
@@ -21,10 +29,12 @@ app.use((_req: Request, res: Response, next: NextFunction) => {
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// ROUTES
+
 app.get('/', (req, res) => {
   res.status(StatusCodes.OK).send('GRX Back-end Challenge');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running at port:${PORT}`);
-});
+// START LISTEN SERVER
+
+startServer(app);
