00618d1e98c0523f33de9391571c51f99e76a4db
Celso Oliva Aparecido <celso.oliva.aparecido@gmail.com>
2022-05-03 23:13:49 -0300

    feat: adds answer controller

---
+++ src/controllers/answer.ts
@@ -0,0 +1,29 @@
+import { RequestHandler } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import AppError from '../errors/AppError';
+import createAnswerSvc from '../services/answer';
+
+type Answer = {
+  [index: string]: string
+};
+
+const createAnswerCtrl: RequestHandler = (async (req, res, next) => {
+  const {
+    question1, question2, question3, question4,
+  } = req.body as Answer;
+  try {
+    await createAnswerSvc({
+      question1, question2, question3, question4,
+    });
+    return res
+      .status(StatusCodes.OK)
+      .json({ message: 'Suas respostas foram armazenadas com sucesso!' });
+  } catch (error) {
+    if (error instanceof AppError) {
+      return next(error);
+    }
+    return next(new Error('Erro interno ao guardar respostas'));
+  }
+}) as RequestHandler;
+
+export default createAnswerCtrl;
