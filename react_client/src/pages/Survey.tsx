0fdea561fb02d809a480e49d2536071a859bf3f5
Celso Oliva Aparecido <celso.oliva.aparecido@gmail.com>
2022-05-11 14:12:50 -0300

    chore(FE): adds SurveyProvider wrapping form component

--- react_client/src/pages/Survey.tsx
+++ react_client/src/pages/Survey.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import usePostAnswers from '../api/usePostAnswers';
 import HeroLayout from '../components/HeroLayout';
 import SurveyForm from '../components/Form/SurveyForm';
 import SurveyStatistics from '../components/Statistics/SurveyStatistics';
 import surveyJSON from '../constants/survey.json';
 import { SuccessResponse, SurveyJSON } from '../types/types';
+import SurveyProvider from '../context/SurveyProvider';
 
 const Survey = () => {
   const postAnswerMutation = usePostAnswers();
+  const [newSurvey, setNewSurvey] = useState<boolean>(true);
   const renderStatistics = (serverState: SuccessResponse) => {
     const { responses } = surveyJSON as SurveyJSON;
     if (postAnswerMutation.error) {
@@ -15,11 +17,9 @@ const Survey = () => {
       return <p className="is-danger">Houve um erro ao processar a requisição</p>;
     }
     return (
-      postAnswerMutation.data && (
-        <SurveyStatistics
-          statistics={serverState}
-          responseLabels={responses}
-        />
+      !newSurvey
+      && serverState && (
+        <SurveyStatistics statistics={serverState} responseLabels={responses} />
       )
     );
   };
@@ -27,9 +27,16 @@ const Survey = () => {
   return (
     <HeroLayout>
       <div className="columns is-centered">
-        <SurveyForm postAnswers={postAnswerMutation} survey={surveyJSON as SurveyJSON} />
+        <SurveyProvider>
+          <SurveyForm
+            postAnswers={postAnswerMutation}
+            survey={surveyJSON as SurveyJSON}
+            newSurvey={newSurvey}
+            setNewSurvey={setNewSurvey}
+          />
+        </SurveyProvider>
       </div>
-      {postAnswerMutation.data && renderStatistics(postAnswerMutation.data)}
+      {postAnswerMutation.isSuccess && renderStatistics(postAnswerMutation.data)}
     </HeroLayout>
   );
 };
